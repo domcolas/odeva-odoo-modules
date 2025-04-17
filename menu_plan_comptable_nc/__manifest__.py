9c1d6da31141daeb1ed4a5f7aaf7629710a784b1
Dominique Colas <cdom@mac.com>
2025-04-17 08:49:10 +0000

    📦 Ajout module Plan Comptable NC

--- menu_plan_comptable_nc/__manifest__.py
+++ menu_plan_comptable_nc/__manifest__.py
@@ -1,11 +1,12 @@
 {
-  "name": "Plan Comptable NC",
-  "version": "17.0.1.0.0",
-  "author": "Odeva",
-  "category": "Accounting",
-  "license": "AGPL-3",
-  "depends": ["account"],
-  "data": ["data/plan_nc.xml"],
-  "installable": True,
-  "auto_install": False,
+    "name": "Plan Comptable NC",
+    "version": "17.0.1.0.0",
+    "author": "Odeva",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": [
+        "data/plan_comptable_nc.xml"
+    ],
+    "installable": True,
+    "license": "AGPL-3"
 }
